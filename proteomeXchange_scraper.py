7aa4ea56c6af3ace16005f32c95fa1c5b7ed39a0
Kimia Rostin <40971424+kim1339@users.noreply.github.com>
2022-05-20 10:00:54 -0700

    supports user-defined keyword list + only scrapes newly added ProteomeXchange datasets

--- proteomeXchange_scraper.py
+++ proteomeXchange_scraper.py
@@ -1,11 +1,9 @@
-import re
+import argparse
 import time
-import numpy as np
 import pandas as pd
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
 def web_table_into_dataframe(soup):
@@ -45,17 +43,34 @@ def get_page_sources(link, waitTime=5, fullDump=True):
     select_object.select_by_value('1000')
     num_pages = int(pager_element.find_element_by_class_name('pagedisplay').get_attribute('value').split("/")[-1])
     
-    full_dataframe = pd.DataFrame()
+    full_dataframe = pd.read_csv('full_metadata_of_PX.tsv', sep='\t')
+    existing_IDs = full_dataframe['Dataset Identifier'].values
 
     for i in range(num_pages):
         soup = BeautifulSoup(d.page_source, features='lxml')
-        full_dataframe = pd.concat([full_dataframe, web_table_into_dataframe(soup)])
+        df = web_table_into_dataframe(soup)
+        df_IDs = df['Dataset Identifier'].values
+        full_dataframe = pd.concat([df, full_dataframe])
         time.sleep(1.5)
-        pager_element.find_elements_by_class_name('next')[0].click()
+        if any(dataset_ID in existing_IDs for dataset_ID in df_IDs):
+            break
+        else:
+            pager_element.find_elements_by_class_name('next')[0].click()
 
     full_dataframe = full_dataframe.drop_duplicates()
     full_dataframe.to_csv('full_metadata_of_PX.tsv', index=False, sep='\t')
     print("Web scraping complete.")
+
+def keyword_matching(data, keyword_list, column2look=['Title', 'Keywords']):
+    binary_matches = pd.DataFrame()
+    for col in column2look:
+        binary_matches[col] = data[col].str.contains((r'\b(?:{})\b'.format('|'.join(keyword_list))))
+    return binary_matches
+
+# get user-defined keyword list from command line using -i or --inputFile flags
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--inputFile", type=str, help="name of text file w/ each keyword on a new line")
+args = vars(parser.parse_args())
 
 # get all datasets from ProteomeXchange
 get_page_sources('http://proteomecentral.proteomexchange.org/cgi/GetDataset')
@@ -68,6 +83,7 @@ full_dataframe['Keywords'] = full_dataframe['Keywords'].str.lower()
 
 filtered_df = full_dataframe[full_dataframe['Species'].str.contains('homo sapiens') | full_dataframe['Species'].str.contains('human')]
 
+# default keyword list
 pediatric_cancer_k_list = ['child', 'pediatric', 'paediatric', 'leukemia', 'medulloblastoma', 'astrocytoma', 'glioma', 'retinoblastoma', 'ewing'
                           , 'osteosarcoma', 'lymphoma', 'all', 'aml', 'rhabdomyosarcoma', 'wilm', 'nephroblastoma', 'glioblastoma', 'ependymoma', 'atrt'
                           , 'oligodendroglioma', 'pnet', 'pineoblastoma', 'rhabdoid', 'craniopharyngioma', 'dnet', 'schwannoma', 'meningioma', 
@@ -75,12 +91,14 @@ pediatric_cancer_k_list = ['child', 'pediatric', 'paediatric', 'leukemia', 'medu
                            'brain cancer', 'brain tumo', 'spinal cord tumo', 'sarcoma', 'thyroid cancer', 'thyroid carcinoma', 'kidney cancer', 'kidney tumo', 'central nervous system tumo',
                            'CNS tumo', 'dysembryoplastic neuroepithelial', 'primitive neuroectodermal', 'teratoid']
 
-def keyword_matching(data, column2look=['Title', 'Keywords'], keyword_list=pediatric_cancer_k_list):
-    binary_matches = pd.DataFrame()
-    for col in column2look:
-        binary_matches[col] = data[col].str.contains((r'\b(?:{})\b'.format('|'.join(keyword_list))))
-    return binary_matches
-    
-filtered_df = filtered_df[keyword_matching(filtered_df).sum(axis=1) > 0]
+# use the user-defined keyword list if provided
+if args["inputFile"] != None:
+    with open(str(args['inputFile'])) as file:
+        lines = file.readlines()
+        keywords = [line.replace('\n', '').lower() for line in lines]
+else:
+    keywords = pediatric_cancer_k_list
+
+filtered_df = filtered_df[keyword_matching(filtered_df, keywords).sum(axis=1) > 0]
 filtered_df.to_csv('filtered_metadata_of_PX.tsv', index=False, sep='\t')
 print("Dataset filtering complete.")
